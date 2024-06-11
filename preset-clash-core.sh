d14391e8e89c4851c2e89809fee05f576c5c5ce2
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-12 06:36:08 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -2,27 +2,14 @@
 #=================================================
 # File name: perest-clash-core.sh
 # System Required: Linux
-# Version: 1.0
+# Version: 1.0 se
 # Lisence: MIT
-# Author: SuLingGG
-# Blog: https://mlapp.cn
+# Author: SuLingGG [edit by xiaouex]
+# Changelog:Download Meta Core only
 #=================================================
 
 echo -e "预置Clash内核"
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-core_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash/core"
-goe_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash"
-
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep "arm64" | awk -F '"' '{print $4}' | grep "v3" )
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-
-wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
-wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
-wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
-
-chmod +x $core_path/clash*
+wget -P /feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/ https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
+wget -P /feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -P /feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
