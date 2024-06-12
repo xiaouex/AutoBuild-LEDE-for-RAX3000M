2a1744875c46d25a85c02d84b58e50f544f8b155
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 01:16:52 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -9,11 +9,12 @@
 #=================================================
 
 echo -e "预置Clash内核"
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
 core_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash/core"
 goe_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash"
 
-wget  https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar xOvz > feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
+wget  https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/core && tar -zxvf feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash-linux-arm64.tar.gz -C feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
+
 wget  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoIP.dat
 wget  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoSite.dat
 
