929d38d4e68a0c303f97dd815860b47805a6756c
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 07:54:29 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,9 +1,9 @@
 #加入argon config
-cd openwrt/package/feeds/kenzo
+cd package/feeds/kenzo
 rm -rf luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 
 #加入新argon
-cd lede/package/feeds/kenzo
+cd package/feeds/kenzo
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
