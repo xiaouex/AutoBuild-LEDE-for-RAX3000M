4dfce47b8e7b9ec81425ca0398c55969ced82d1b
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-12 18:33:49 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -11,5 +11,7 @@
 echo -e "预置Clash内核"
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta/ https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
+cd feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
+tar -xzvf clash-linux-arm64.tar.gz && rm clash-linux-arm64.tar.gz
 wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
