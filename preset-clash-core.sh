6a99bf936dcfcb52c728f3a2f4a15b3c7807f22a
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-12 22:39:41 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -10,8 +10,11 @@
 
 echo -e "预置Clash内核"
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta/ https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
-cd feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta
-tar -xzvf clash-linux-arm64.tar.gz && rm clash-linux-arm64.tar.gz
-wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget -P feeds/luci/applications/luci-app-openclash/root/etc/openclash/ https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+core_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash/core"
+goe_path="feeds/luci/applications/luci-app-openclash/root/etc/openclash"
+
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar xOvz > $core_path/clash_meta
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > $goe_path/GeoSite.dat
+
+chmod +x $core_path/clash*
