990143e8974b8f838a58e444854855b909e89ddc
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 07:33:07 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,1 +1,9 @@
+#加入argon config
+cd lede/package/lean
+rm -rf luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 
+#加入新argon
+cd lede/package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
