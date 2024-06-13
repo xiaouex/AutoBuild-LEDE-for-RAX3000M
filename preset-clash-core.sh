e81f5333ccfbd6dca9fd8609aea413c2bfd95b07
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 19:36:28 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -13,7 +13,7 @@ mkdir -p $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/ro
 core_path="$GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/root/etc/openclash/core"
 goe_path="$GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/root/etc/openclash"
 
-wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz | tar -zOxvf > $core_path/clash_meta
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz | tar -xOvz > $core_path/clash_meta
 
 wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
 wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > $goe_path/GeoSite.dat
