50fd56966e37b37e241b478778a3877d465314a9
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 22:47:31 +0800

    Delete diy-part3.sh

--- diy-part3.sh
+++
@@ -1,7 +0,0 @@
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-theme-argon
-
-rm -rf $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-argon-config
-
-
