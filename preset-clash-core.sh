0d43a94da0f84c1b0854d56bc920730574ae2393
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 19:34:10 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -13,9 +13,9 @@ mkdir -p $GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/ro
 core_path="$GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/root/etc/openclash/core"
 goe_path="$GITHUB_WORKSPACE/openwrt/feeds/luci/applications/luci-app-openclash/root/etc/openclash"
 
-wget  https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -P $core_path && tar -zxvf $core_path/clash-linux-arm64.tar.gz > $core_path/clash_meta
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz | tar -zOxvf > $core_path/clash_meta
 
-wget  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
-wget  https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > $goe_path/GeoSite.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > $goe_path/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > $goe_path/GeoSite.dat
 
 chmod +x $core_path/clash*
