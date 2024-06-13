dc5444e11f0fcd8fda848bea4e67528cd62c5ec5
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-06-13 18:55:52 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,8 +1,8 @@
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git $GITHUB_WORKSPACE/openwrt/package/lean/luci-theme-argon
 
-rm -rf feeds/kenzo/luci-theme-argon
+rm -rf $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-theme-argon
 
 
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git $GITHUB_WORKSPACE/openwrt/package/lean/luci-app-argon-config
 
-rm -rf feeds/kenzo/luci-app-argon-config
+rm -rf $GITHUB_WORKSPACE/openwrt/feeds/kenzo/luci-app-argon-config
