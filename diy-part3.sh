6518cf10b970bc0dffd5f0ea896d6d8bbcec820f
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-07-06 09:56:03 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,5 +1,4 @@
-./scripts/feeds install -a && sed -i 's/PACKAGE_luci-lib-ipkg/PACKAGE_luci-base/g' $GITHUB_WORKSPACE/openwrt/package/feeds/small/luci-app-ssr-plus/Makefile
-
+./scripts/feeds install -a 
 #&& rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 #rm -rf feeds/packages/utils/v2dat
