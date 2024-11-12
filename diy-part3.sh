c9aa5806fd705b6676a660c369b3f47d79bee02a
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-11-13 00:46:22 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -2,5 +2,5 @@
 #&& rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 #rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
