9bc2c709e53f06200bf1d6eabb13f3150c06e78b
xiaouex <36121363+xiaouex@users.noreply.github.com>
2024-11-25 12:39:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/MineRouter/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/MineRouter/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
